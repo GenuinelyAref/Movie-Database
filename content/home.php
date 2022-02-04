9467b77a8d25ef7119f3b8513510edacdda10114
GenuinelyAref <osmana69529@masseyhigh.school.nz>
2022-02-04 23:56:20 +1300

    update home page to contain relevant info
    * remove irrelevant info from previous outcome and update with relevant details/links

--- content/home.php
+++ content/home.php
@@ -14,18 +14,18 @@
     </p>
 
     <ul>
-        <li>browse through <a href="index.php?page=showall">all the quotes</a></li>
-        <li>grab <a href="index.php?page=random">ten random quotes</a> </li>
-        <li>See quotes that have <a href="index.php?page=recent">recently been added</a> to this database.</li>
+        <li>browse through <a href="index.php?page=showall">all the movies</a></li>
+        <li>grab <a href="index.php?page=random">ten random movies</a></li>
+        <li>See movies that have <a href="index.php?page=recent">recently been added</a> to this database.</li>
     </ul>
 
     <p>
-        <i>For each quote, you can click on the tags underneath it to find related quotes.  If you click on the author's name, all the quotes by that author will be displayed.</i>
+        <i>For each movie, you can click on the genre tags underneath it to find movies under the same genre. If you click on the director's name, all the movies by that director will be displayed.</i>
     </p>
 
 
 <h2>Acknowledgments</h2>
 
-<p>The quotes that make up the bulk of this database are in the public domain.  The <a href="https://www.kaggle.com/cerolacia/final-quotes-scraped">source of the data</a> for this database is at the preceding link.</p>
+<p>The movies that make up the bulk of this database are in the public domain.  The <a href="https://www.kaggle.com/vipulgote4/oscars-nominated-movies-from-2000-to-2017">source of the data</a> for this database is at the preceding link.</p>
 
-<p>The author details were sourced from various author pages via <a href="https://en.wikipedia.org/wiki/Main_Page">Wikipedia</a>.</p>
+<p>The director details were sourced from various director pages via <a href="https://en.wikipedia.org/wiki/Main_Page">Wikipedia</a> but also via many <a href="https://www.imdb.com/">IMDb pages</a>.</p>
