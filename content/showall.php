933f8bcd206ebb9f91b5022a3bd88d80d8f8c17c
GenuinelyAref <osmana69529@masseyhigh.school.nz>
2022-02-08 12:48:58 +1300

    display nominations and awards of each data entry

--- content/showall.php
+++ content/showall.php
@@ -106,6 +106,51 @@ do {
           <!-- line break -->
           <br />
 
+
+          <?php
+          // Nominations
+          $nominations = $find_rs['Nominations'];
+          if ($nominations > 1) {
+            ?>
+            <b><?php echo $nominations; ?></b> Nominations
+            <?php
+          }
+          elseif ($nominations == 1) {
+            ?>
+            <b>1</b> Nomination
+            <?php
+          }
+          else {
+            ?>
+            <b>No</b> Nominations
+            <?php
+          }
+
+          // join nominations and awards on same line
+          echo " and ";
+
+          // Awards
+          $awards = $find_rs['Awards'];
+          if ($awards > 1) {
+            ?>
+            <b><?php echo $awards; ?></b> Awards
+            <?php
+          }
+          elseif ($awards == 1) {
+            ?>
+            <b>1</b> Award
+            <?php
+          }
+          else {
+            ?>
+            <b>no</b> Awards
+            <?php
+          }
+          ?>
+
+
+        </p>
+
         <!-- genre tags go here -->
         <?php include 'show_genres.php'; ?>
 
